6b44a901afa9eddf16822ffec31275e5bed468b7
Tyler Glaze <glaze_tr@yahoo.com>
2023-03-07 19:13:30 -0700

    added logic around appending the base url to urls without it

--- src/utilities/crawlURL.js
+++ src/utilities/crawlURL.js
@@ -5,18 +5,24 @@ function parseHtmlForHREFTags(htmlFile, url) {
   // parses all elements in html for urls with partial match to mainDomain then puts links into linksQueue array and seenLinks array to prevent duplicates then console logs and returns the linksQueue array
   htmlFile("a").each(function () {
     const href = htmlFile(this).attr("href");
-    if (href) {
-      if (
+    if (
         href.includes(mainDomain) ||
         (!/^https?:\/\//i.test(href) && !seenLinks.includes(href))
       ) {
-        // for now I am combining so we have the full url instead of just like /contacts
+     if (
+        (!/^https?:\/\//i.test(href))
+    )
+        { // for now I am combining so we have the full url instead of just like /contacts
         linksQueue.push(url + href);
         seenLinks.push(href);
-      }
-    }
-  });
-  return linksQueue;
+    } 
+    else {
+        linksQueue.push(href);
+        seenLinks.push(href);
+   }
 }
-
+    })
+    return linksQueue;
+  };
+  
 module.exports = parseHtmlForHREFTags;
